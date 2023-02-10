61bff245dbe8da4c4046f5c5d4977915ed17e841
oov <ilovevipper@gmail.com>
2023-02-11 02:51:49 +0900

    fix error when compiling with Clang 15

--- src/aviutl.h
+++ src/aviutl.h
@@ -3,4 +3,16 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+#ifdef __GNUC__
+#  ifndef __has_warning
+#    define __has_warning(x) 0
+#  endif
+#  pragma GCC diagnostic push
+#  if __has_warning("-Winvalid-utf8")
+#    pragma GCC diagnostic ignored "-Winvalid-utf8"
+#  endif
+#endif // __GNUC__
 #include "aviutl_sdk/filter.h"
+#ifdef __GNUC__
+#  pragma GCC diagnostic pop
+#endif // __GNUC__
